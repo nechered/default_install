eb875d0c0d1d904c06b3b6bb57abfb42dda197ab
nechered <nelson.cheuque@protonmail.com>
2017-10-22 19:33:17 +0200

    finished

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@
 #v0.1 2017-10-22
 #
 
-
 echo "
 ##########################################################
 # Default Debian / Ubuntu install Script                 #
@@ -33,14 +32,41 @@ cd ~/
 mkdir .antigen
 cp antigen.zsh .antigen/
 touch .zshrc
+cat >> .zshrc << EOF
+source ~/.antingen/antigen.zsh
 
+# Load the oh-my-zsh's library.
+antigen use oh-my-zsh
+
+# Bundles from the default repo (robbyrussell's oh-my-zsh).
+antigen bundle git
+antigen bundle heroku
+antigen bundle pip
+antigen bundle lein
+antigen bundle command-not-found
+
+# Syntax highlighting bundle.
+antigen bundle zsh-users/zsh-syntax-highlighting
+
+# Load the theme.
+antigen theme candy
+
+# Tell Antigen that you're done.
+antigen apply
+
+# GPG-card for SSH
+GPG_TTY=$(tty); export GPG_TTY;
+if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+  export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
+fi
+gpg-connect-agent /bye
+EOF
 
 echo "###Repo time"
 mkdir repos
 cd repos
 git clone https://github.com/secrary/SSMA.git
 git clone https://github.com/VirusTotal/yara.git
-
 
 # Let's clean up apt
 sudo apt-get autoclean -y
